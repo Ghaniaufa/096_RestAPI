6e43fda23edade5b34d659381f605f762e948ca1
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-27 18:09:53 +0700

    add Composable DestinasiHome to Route HomeScreen

--- app/src/main/java/com/example/consumeapi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/consumeapi/navigation/PengelolaHalaman.kt
@@ -1,21 +1,38 @@
 package com.example.consumeapi.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.consumeapi.ui.kontak.screen.DestinasiEntry
+import com.example.consumeapi.ui.kontak.screen.EntryKontakScreen
 import com.example.consumeapi.ui.screens.DestinasiHome
+import com.example.consumeapi.ui.screens.HomeScreen
 
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
 
   NavHost(
     navController = navController,
     startDestination = DestinasiHome.route,
-    modifier = Modifier,
-  ){
+    modifier = Modifier
+  ) {
+
+    composable(DestinasiHome.route) {
+      HomeScreen(navigatetoItemEntry = {
+        navController.navigate(
+          (DestinasiEntry.route)
+        )
+      })
+    }
+
 
   }
-
 }
 
