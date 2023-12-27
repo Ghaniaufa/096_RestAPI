ce585b9604ce6b9813a974a47065e1520612f20a
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-27 11:34:24 +0700

    create data class InsertUiEvent from InsertViewModel for id,nama,email,nohp

--- app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -32,3 +32,10 @@ class InsertViewModel(private val kontakRepository: KontakRepository): ViewModel
 data class InsertUistate(
     val insertUiEvent: InsertUiEvent = InsertUiEvent(),
 )
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = "",
+)
