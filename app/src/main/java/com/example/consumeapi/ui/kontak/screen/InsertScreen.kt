f7a459283b36b3040ab7d025d9ecd13787e3ab53
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-27 11:59:01 +0700

    create OutlinedTextField with Column for No Telpon

--- app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
@@ -1,9 +1,19 @@
 package com.example.consumeapi.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.consumeapi.ui.home.viewmodel.InsertUiEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
     insertUiEvent: InsertUiEvent,
@@ -11,5 +21,35 @@ fun FormInputSiswa(
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
 ){
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it)) },
+            label = {Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.email,
+            onValueChange = {onValueChange(insertUiEvent.copy(email = it)) },
+            label = {Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nohp,
+            onValueChange = {onValueChange(insertUiEvent.copy(nohp = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = {Text("No HP") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
 
+    }
 }
