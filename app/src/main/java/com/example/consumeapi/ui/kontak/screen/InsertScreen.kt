b6790d4952349e5ce340d159861c2c646aa6d57d
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-27 11:53:08 +0700

    create new file InsertScreen at Kontak>Screen

---
+++ app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,15 @@
+package com.example.consumeapi.ui.kontak.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.consumeapi.ui.home.viewmodel.InsertUiEvent
+
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+
+}
